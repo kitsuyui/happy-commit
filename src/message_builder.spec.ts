2603980c4d8bdd372954eae8825c8397fa8b2f40
kitsuyui <kitsuyui@kitsuyui.com>
2025-06-20 12:39:18 +0900

    Migrate to vitest from jest
    - Install vitest
    - Remove jest
    - Update test scripts / files
    
    I already use vitest instead of jest also in my other projects.

--- src/message_builder.spec.ts
+++ src/message_builder.spec.ts
@@ -1,3 +1,5 @@
+import { describe, expect, it } from 'vitest'
+
 import { CustomMessageBuilder } from './message_builder'
 
 describe('MessageBuilder', () => {
---
+++ vitest.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    /**
+     * globals: true allows you to use describe, test, etc. without importing them
+     * It is better to import them explicitly to avoid future liabilities, so it is not set to globals: true
+     */
+    globals: false,
+    exclude: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/build/**',
+      '**/coverage/**',
+      "**/.git/**",
+    ]
+  },
+  /**
+   * clearScreen: true clears the screen when running tests. The default is true
+   * If you are watching multiple processes, the results of other processes will also be cleared, so it is set to false
+   */
+  clearScreen: false,
+})
